1df969e247b7f2d0fcdd7ade28ffc4c1d9d1de93
minkle saini <minkle7.in@gmail.com>
2021-05-16 00:37:31 -0700

    add line

--- exception.py
+++ exception.py
@@ -1,6 +1,12 @@
+import sys
 x = int(input("x: "))
 y = int(input("y: "))
 
-result = x / y
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("error cannot be divide by 0.")
+    sys.exit(10)    
+
 
 print (f"{x} /{y} = {result} ")
